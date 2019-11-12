5e1eb78d152c5aefc8bd5dc3993b4b20428e4d22
mrzhqiang <287431404@qq.com>
2019-11-12 18:25:23 +0800

    提交：初始化项目

---
+++ src/main/java/randall/dbserver/DBServer.java
@@ -0,0 +1,57 @@
+package randall.dbserver;
+
+import java.net.URL;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import randall.dbserver.util.Dialogs;
+import randall.dbserver.util.Monitor;
+
+/**
+ * @author mrzhqiang
+ */
+public final class DBServer extends Application {
+  public static void main(String[] args) {
+    // todo 返回消息给游戏控制台
+    launch(args);
+  }
+
+  private static final String APP_TITLE = "数据库服务器";
+  private static final URL APP_FXML = DBServer.class.getResource("application.fxml");
+  private static final URL APP_CSS = DBServer.class.getResource("application.css");
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle(APP_TITLE);
+    Monitor monitor = Monitor.getInstance();
+    monitor.record("准备启动程序..");
+    try {
+      FXMLLoader loader = new FXMLLoader(APP_FXML);
+      Scene scene = new Scene(loader.load());
+      scene.getStylesheets().add(APP_CSS.toExternalForm());
+      primaryStage.setScene(scene);
+      primaryStage.setOnCloseRequest(event -> {
+        Dialogs.confirm("是否确认关闭数据库服务器？")
+            .ifPresent(buttonType -> {
+              stop();
+              Platform.exit();
+            });
+      });
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    monitor.report("程序启动完毕！");
+  }
+
+  @FXML void initialize() {
+
+  }
+
+  @Override
+  public void stop() {
+  }
+}
